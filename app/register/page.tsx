bf573b1ebfe933407d9cedea5eebad818b44f38f
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-12-14 21:06:32 +0800

    update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,31 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section className="mt-8 h-full w-full flex flex-col justify-center items-center">
+      <h1 className="text-center text-primary text-4xl">Register</h1>
+      <form className="w-[500px] flex flex-col justify-center items-center gap-y-6 mt-10 px-10">
+        <Input placeholder="Email" className="placeholder:text-gray-400" />
+        <Input placeholder="Password" className="placeholder:text-gray-400" />
+        <Link
+          href="/register"
+          className={cn("w-full rounded-full", buttonVariants())}
+        >
+          Register
+        </Link>
+        <p className="text-gray-400 text-sm">or login using:</p>
+        <Button className="flex gap-x-4 bg-white text-black border-2 border-gray-300 rounded-full w-full hover:text-white">
+          <Image src="/google.png" alt="google" height={16} width={16} /> Login
+          with Google
+        </Button>
+      </form>
+    </section>
+  );
 };
 
 export default page;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,23 +6,23 @@ import { cn } from "@/lib/utils";
 const Navbar = () => {
   return (
     <header className="flex items-center justify-between max-w-4xl mx-auto">
-      <nav className="flex w-full justify-between items-center gap-8 text-gray-500 font-semibold">
+      <nav className="flex w-full justify-between items-center gap-8 text-gray-500 font-semibold px-10">
         <div className="flex gap-x-8">
-          <a href="" className="text-[#F97316] font-semibold text-2xl">
+          <a href="/" className="text-[#F97316] font-semibold text-2xl">
             ST PIZZA
           </a>
-          <div className="flex items-center gap-x-6 ">
-            <Link href="">Home</Link>
+          <div className="hidden md:flex items-center gap-x-6 ">
+            <Link href="/">Home</Link>
             <Link href="">Menu</Link>
             <Link href="">About</Link>
             <Link href="">Contact</Link>
           </div>
         </div>
-        <div className="flex">
+        <div className="hidden md:flex">
           <Link href="/" className="py-2 px-4">
             Login
           </Link>
-          <Link href="/" className={cn("w-[80px]", buttonVariants())}>
+          <Link href="/register" className={cn("w-[80px]", buttonVariants())}>
             Register
           </Link>
         </div>
